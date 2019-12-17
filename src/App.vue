b4748cb78b4bc69e6db5828291c3da4f910abe5c
conan4069 <mevr02@gmail.com>
2019-12-17 03:14:10 -0400

    Version de vuetify de mi curriculum
    mend

---
+++ src/App.vue
@@ -0,0 +1,100 @@
+<template>
+  <v-app app>
+    <!-- <v-app-bar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        text
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>fas fa-external-link-alt</v-icon>
+      </v-btn>
+    </v-app-bar> -->
+ <v-navigation-drawer
+    app
+    permanent
+    left
+    color="blue-grey darken-4"
+  >
+    <template v-slot:prepend>
+      <v-row class="ml-5" align="center" justify="center">
+        <v-col cols="12" align-self="center">
+          <v-avatar size="164">
+            <img :style="`border:5px solid #607d8b !important`" :src="require('@/assets/IMG_20191126_082725.jpg')">
+          </v-avatar>
+        </v-col>
+        <v-col cols="12" align-self="center">
+          <span class="white--text headline font-weight-bold">
+            Miguel Villamizar
+          </span>
+        </v-col>
+      </v-row>
+    </template>
+
+        <v-divider></v-divider>
+
+        <v-list dense>
+          <v-list-item
+            dark
+            :href="item.link ? item.link : '#'"
+            color="white"
+            v-for="item in items"
+            :key="item.title"
+          >
+            <v-list-item-icon class="mr-1">
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+    </v-navigation-drawer>
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+  data(){
+    return {
+      items:[
+        {
+          link: "https://www.linkedin.com/in/miguel-villamizar-9b653a145/",
+          icon: "fab fa-linkedin-in",
+          title: "/miguel-villamizar-9b653a145/"
+        },
+        {
+          link: "https://conan4069.gitlab.io/",
+          icon: "fab fa-gitlab",
+          title: "@conan4069"
+        },
+        {
+          link: "https://conan4069.github.io/",
+          icon: "fab fa-github",
+          title: "@conan4069"
+        },
+        {
+          link: "#",
+          icon: "fa fa-user",
+          title: "+58 412 078 0581"
+        },
+      ]
+    }
+  },
+};
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,388 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12" md="6">
+        <h2 class="mb-5 text-center blue-grey--text darken-1 headline font-weight-bold">
+          Acerca de mi
+        </h2>
+        Soy un Ingeniero en Sistema, apasionado por la programacion y las distintas tegnologias 
+        he tenido oportunidad de trabajar proyectos con OdooERP, Django, Vuejs y Laravel, 
+        y el manejo de base de datos con mysql, postgres. He trabajado usando 
+        repositorios en Gitlab y Github.
+        <br>
+        <h2 class="mb-5 text-center blue-grey--text darken-1 headline font-weight-bold">Pasatiempos</h2>
+          <p>Escuchar musica(Rock,pop, musica clasica), ver series o peliculas de ciencia ficción, ver series animadas japonesas</p>
+          <p class="mb-0">Me gusta investigar sobre otros metodos para solucion de problemas en las distintos frameworks</p>
+      </v-col>
+      <v-col cols="12" md="6">
+        <h2 class="mb-5 text-center blue-grey--text darken-1 headline font-weight-bold">
+          Estudios
+        </h2>
+        <v-col cols="12">
+          <v-icon small class="fa fa-trophy amber--text darken-1 mr-5"></v-icon>
+          <span>
+           <b>Instituto:</b> Universidad Nacional Experimental Politécnica de la Fuerza Armada Bolivariana
+          </span> <br>
+          <span class="ml-9">
+            <b>Titulo:</b> Ingeniero de Sistemas
+          </span> <br>
+          <span class="ml-9">
+            <b>Fechas:</b> Septiembre 2012 - Diciembre 2017
+          </span>
+        </v-col>
+        <v-col cols="12">
+          <v-icon small class="fa fa-trophy amber--text darken-1 mr-5"></v-icon>
+          <span>
+           <b>Instituto:</b> Unidad Educativa Francisco de Paula Andrade
+          </span> <br>
+          <span class="ml-9">
+            <b>Titulo:</b> Bachiller en ciencias
+          </span> <br>
+          <span class="ml-9">
+            <b>Fechas:</b> Septiembre 2007 - Junio 2012
+          </span>
+        </v-col>
+      </v-col>
+      <v-col cols="12">
+        <h2 class="mb-5 text-center blue-grey--text darken-1 headline font-weight-bold">
+          Experiencia
+        </h2>
+        He trabajado realizando modulos en OdooERP:
+        <ul>
+          <li>
+            Control de acceso: controlaba la entrada y salida de empleados y visitantes a las instalaciones.
+          </li>
+          <li>
+            Apoyo en modulo de Requisiciones: Cada departamento podia realizar o notificar pedidos para la organizacion y se hacia seguimiento de cada canal por el que pasaba el pedido hasta el retiro de los almacenes del trabajador solicitante
+          </li>
+          <li>
+            Administracion de contrato: Manejo de las cotizaciones de una empresa.
+          </li>
+          <li>
+            Operaciones de mantenimiento: Complemento del de administracion de contrato dependiendo del carro que llegaba se cotizaba lo que se le realizaba y se descontaba del contrato.
+          </li>
+          <li>
+            Nomina: modulo para manejo de nomina en los RRHH.
+          </li>
+        </ul>
+
+        En sistemas como la primer y segunda version del iprintpos:
+          Sistema para manejo de una empresa de imprenta, realizacion de ordenes de compra como de venta, para ofrecer un servicio a distintas empresas para el manejo de las imprentas. <br>
+          https://app.iprintpos.com/login <br>
+          http://eprintpos.com. <br>
+        Apoyo en el sistema de clubmercado: Un ecommerce para venta de verduras y carnes. <br>
+        Apoyo en el sistema de Akicoins: Un sistema para compra y venta de cryptomoneda br
+        Apoyo en el sistema STV: Un sistema para ayudar al cobro del seguro social con la posibilidad de trabajar offline desde el telefono y poder enviar la informacion al tener internet.
+      </v-col>
+      <v-col cols="12" md="6">
+
+        <h2 class="mb-5 text-center blue-grey--text darken-1 headline font-weight-bold">Skills</h2>
+        <v-row class="ml-5">
+          <v-col cols="3">
+            <v-icon class="fa fa-check success--text"></v-icon>
+            Odoo
+          </v-col>
+          <v-col cols="3">
+            <v-icon class="fa fa-check success--text"></v-icon>
+            Vuejs
+          </v-col>
+          <v-col cols="3">
+            <v-icon class="fa fa-check success--text"></v-icon>
+            Laravel
+          </v-col>
+          <v-col cols="3">
+            <v-icon class="fa fa-check success--text"></v-icon>
+            Django
+          </v-col>
+          <v-col cols="3">
+            <v-icon class="fa fa-check success--text"></v-icon>
+            Photoshop
+          </v-col>
+          <v-col cols="3">
+            <v-icon class="fa fa-check success--text"></v-icon>
+            Inkscape
+          </v-col>
+        </v-row>
+        <v-row class="ml-5">
+          <v-col cols="3">
+            <v-icon large class="fab fa-html5"></v-icon>
+          </v-col>
+          <v-col cols="3">
+            <v-icon large class="fab fa-js-square"></v-icon>
+          </v-col>
+          <v-col cols="3">
+            <v-icon large class="fab fa-git"></v-icon>
+          </v-col>
+          <v-col cols="3">
+            <v-icon large class="fab fa-node-js"></v-icon>
+          </v-col>
+          <v-col cols="3">
+            <v-icon large class="fab fa-php"></v-icon>
+          </v-col>
+          <v-col cols="3">
+            <v-icon large class="fab fa-python"></v-icon>
+          </v-col>
+          <v-col cols="3">
+            <v-icon large class="fab fa-vuejs"></v-icon>
+          </v-col>
+          <v-col cols="3">
+            <v-icon large class="fab fa-css3-alt"></v-icon>
+          </v-col>
+        </v-row>
+      </v-col>
+      <v-col cols="12" md="6">
+        <h2 class="mb-5 text-center blue-grey--text darken-1 headline font-weight-bold">Certificados</h2>
+        <v-row>
+          <v-col cols="12">
+            <v-icon small class="fa fa-trophy amber--text darken-1"></v-icon>
+            Curso de Desarrollo Web con Php Básico, 2017
+          </v-col>
+          <v-col cols="12">
+            <v-icon small class="fa fa-trophy amber--text darken-1"></v-icon>
+            Curso de Desarrollo con Python Intermedio, 2017
+          </v-col>
+          <v-col cols="12">
+            <v-icon small class="fa fa-trophy amber--text darken-1"></v-icon>
+            1er Congreso: Proyecciones de la tecnología para el desarrollo sostenible,
+            Universidad Nacional Experimental Politécnica de la Fuerza Armada, 2016
+          </v-col>
+          <v-col cols="12">
+            <v-icon small class="fa fa-trophy amber--text darken-1"></v-icon>
+            Congreso de Tecnología libres, 2016
+          </v-col>
+          <v-col cols="12">
+            <v-icon small class="fa fa-trophy amber--text darken-1"></v-icon>
+            Curso de Soporte Técnico básico, 2015
+          </v-col>
+          <v-col cols="12">
+            <v-icon small class="fa fa-trophy amber--text darken-1"></v-icon>
+            Foro Investigación y Desarrollo en Tecnologías Libres, 2016
+          </v-col>
+          <v-col cols="12">
+            <v-icon small class="fa fa-trophy amber--text darken-1"></v-icon>
+            Curso de Python, 2015
+          </v-col>
+          <v-col cols="12">
+            <v-icon small class="fa fa-trophy amber--text darken-1"></v-icon>
+            Curso de Scratch, 2014
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row> 
+<!-- <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="experience">
+        <div class="my-auto">
+          <h2 class="mb-5 text-center">Experiencia</h2>
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">Analiticom</h3>
+              <div class="subheading mb-3">
+                Colaboración en los proyectos para la empresa
+              </div>
+              <p>
+                <div class="my-auto">
+                  <h4 class="mb-2 text-center">STV - IVSS (Frontend-VUE)</h4>
+                  <ul class="fa-ul mb-0">
+                    <li>
+                      <i class="fa-li fa fa-check text-warning"></i>
+                      Modelado de los formularios en el área movil
+                    </li>
+                    <li>
+                      <i class="fa-li fa fa-check text-warning"></i>
+                      Manejo de la aplicación en modo offline, para cumplimiento de los requerimientos
+                    </li>
+                  </ul>
+                </div>
+                <h4 class="mb-2 text-center">Clubmercado v2</h4>
+                  <ul class="fa-ul mb-0">
+                    <li>
+                      <i class="fa-li fa fa-check text-warning"></i>
+                      Funcionalidades en el frontend
+                    </li>
+                  </ul>
+              </p>
+            </div>
+            <div class="resume-date text-md-right">
+              <span>Julio 2018 - Present</span>
+            </div>
+          </div>
+
+
+
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">Sistemas web</h3>
+              <div class="subheading mb-3">
+                Colaboración en la elaboración de sistemas(Backend y Frontend) para Area862
+              </div>
+              <p>
+                <div class="my-auto">
+                  <h4 class="mb-2 text-center">Iprintpos(Laravel con vuejs)</h4>
+                  <ul class="fa-ul mb-0">
+                    <li>
+                      <i class="fa-li fa fa-check text-warning"></i>
+                      Realización de dashboard para visualización de las ordenes por cliente, para mostrar en que estados estan las ordenes y que diseñador esta a cargo del diseño.
+                    </li>
+                    <li>
+                      <i class="fa-li fa fa-check text-warning"></i>
+                      Creación del módulo de Facturas para manejo de pagos de las ordenes
+                    </li>
+                    <li>
+                      <i class="fa-li fa fa-check text-warning"></i>
+                      Realización de los reportes de productos en el inventario, compra de productos por proveedor, productos disponibles, facturas, ordenes, reporte de todas las facturas en un rango de fecha con visualizacion de las ganancias, y gastos totales.
+                    </li>
+                    <li>
+                      <i class="fa-li fa fa-check text-warning"></i>
+                      Manejo de los proveedores, compra y realización de pedidos a los respectivos clientes, creacion de la pantalla para comparacion de precios entre los proveedores que ofrecen algun producto y el manejo de las ordenes con el proveedor, si ya se realizacion de pago. 
+                    </li>
+                  </ul>
+                  <h4 class="mb-2 text-center">Web para area862, con la base del iprintpos system</h4>
+                  <ul class="fa-ul mb-0">
+                    <li>
+                      <i class="fa-li fa fa-check text-warning"></i>
+                      Realización del ecommerce para area.
+                    </li>
+                    <li>
+                      <i class="fa-li fa fa-check text-warning"></i>
+                      Muestreo de categorias, muestreo de productos, modulo para crear ofertas por producto.
+                    </li>
+                    <li>
+                      <i class="fa-li fa fa-check text-warning"></i>
+                      Envio de correos en el area de contactanos.
+                    </li>
+                  </ul>
+                  <h4 class="mb-2 text-center">Administrador para area862, para vender y ofrecer dominios del iprintpost system</h4>
+                  Sistema para vender el sistema iprintpos, con creacion de subdominios por cliente con el sistema de iprintpos.
+                </div>
+              </p>
+            </div>
+            <div class="resume-date text-md-right">
+              <span >Marzo 2018 - Present</span>
+            </div>
+          </div>
+
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">Dai</h3>
+              <div class="subheading mb-3">Realización de modulos de odoo con la empresa dai</div>
+              <p>
+                Empresas a las que se le hicieron modulos
+                <div class="my-auto">
+                  <h4 class="mb-2 text-center">Alianza GLC</h4>
+                  <ul class="fa-ul mb-0">
+                    <li>
+                      <i class="fa-li fa fa-check text-warning"></i>
+                      Módulo para manejo de requisiciones(Solicitudes por departamento), para la compra de productos
+                    </li>
+                    <li>
+                      <i class="fa-li fa fa-check text-warning"></i>
+                      Modificación del módulo de almacenes de odoo
+                    </li>
+                    <li>
+                      <i class="fa-li fa fa-check text-warning"></i>
+                      Modificacion del modulo de finanzas en el area de VAT para adecuarlo al rif venezolano
+                    </li>
+                    <li>
+                      <i class="fa-li fa fa-check text-warning"></i>
+                      Modulo para impresion de cheques
+                    </li>
+                  </ul>
+                  <h4 class="mb-2 text-center">Inmerca</h4>
+                  <ul class="fa-ul mb-0">
+                    <li>
+                      <i class="fa-li fa fa-check text-warning"></i>
+                      Módulo de administración de contratos
+                    </li>
+                    <li>
+                      <i class="fa-li fa fa-check text-warning"></i>
+                      Modulo para valuaciones(Control de ventas con una empresa, con los precios acordados con la empresa), modulo de mantenimiento de vehiculos
+                    </li>
+                    <li>
+                      <i class="fa-li fa fa-check text-warning"></i>
+                      Modulo para generacion de reportes en excel
+                    </li>
+                    <li>
+                      <i class="fa-li fa fa-check text-warning"></i>
+                      Modulos para envio de correos
+                    </li>
+                  </ul>
+                </div>
+              </p>
+            </div>
+            <div class="resume-date text-md-right">
+              <span>Agosto 2017 - Marzo 2018</span>
+            </div>
+          </div>
+
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">Pasantias</h3>
+              <div class="subheading mb-3">Migracion de una aplicacion a odoo</div>
+              <p>Realización de la migracion de un modulo de control de visitas a la empresa.</p>
+            </div>
+            <div class="resume-date text-md-right">
+              <span >Marzo - Julio 2017</span>
+            </div>
+          </div>
+
+        </div>
+
+      </section> -->
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    ecosystem: [
+      {
+        text: 'vuetify-loader',
+        href: 'https://github.com/vuetifyjs/vuetify-loader',
+      },
+      {
+        text: 'github',
+        href: 'https://github.com/vuetifyjs/vuetify',
+      },
+      {
+        text: 'awesome-vuetify',
+        href: 'https://github.com/vuetifyjs/awesome-vuetify',
+      },
+    ],
+    importantLinks: [
+      {
+        text: 'Documentation',
+        href: 'https://vuetifyjs.com',
+      },
+      {
+        text: 'Chat',
+        href: 'https://community.vuetifyjs.com',
+      },
+      {
+        text: 'Made with Vuetify',
+        href: 'https://madewithvuejs.com/vuetify',
+      },
+      {
+        text: 'Twitter',
+        href: 'https://twitter.com/vuetifyjs',
+      },
+      {
+        text: 'Articles',
+        href: 'https://medium.com/vuetify',
+      },
+    ],
+    whatsNext: [
+      {
+        text: 'Explore components',
+        href: 'https://vuetifyjs.com/components/api-explorer',
+      },
+      {
+        text: 'Select a layout',
+        href: 'https://vuetifyjs.com/layout/pre-defined',
+      },
+      {
+        text: 'Frequently Asked Questions',
+        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
+      },
+    ],
+  }),
+};
+</script>
